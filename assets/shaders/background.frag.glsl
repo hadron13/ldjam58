719bffefd0f7a67fe24aa6a15cbc365434f5f00b
hadron13 <carlosbpfonseca@gmail.com>
2025-10-04 14:09:33 -0300

    add the shaders

--- assets/shaders/background.frag.glsl
+++ assets/shaders/background.frag.glsl
@@ -2,6 +2,13 @@
 in vec2 TexCoord;
 out vec4 FragColor;
 uniform sampler2D tex;
+
+uniform vec2 resolution;
+
+
 void main() {
-    FragColor = texture(tex, TexCoord);
+
+
+
+    FragColor = vec4(0.5, 0.5, 0.5, 1.0);
 };
--- assets/shaders/quad.vert.glsl
+++ assets/shaders/quad.vert.glsl
@@ -4,8 +4,10 @@ layout (location = 0) in vec2 aPos;
 layout (location = 1) in vec2 aTexCoord;
 out vec2 TexCoord;
 
+uniform vec2 resolution;
+uniform vec4 rect;
 
 void main(){
     TexCoord = aTexCoord;
-    gl_Position = vec4(aPos, 0.0, 1.0);
+    gl_Position = vec4((aPos * rect.zw/resolution) - 1.0 + ((rect.xy+rect.zw)/resolution), 0.0, 1.0);
 }
