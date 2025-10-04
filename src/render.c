f77301454380899d52c79aea896d102e3a1d0aad
GiovanyH <henriquegiovany677@gmail.com>
2025-10-03 21:29:14 -0300

    render.c prototype

---
+++ src/render.c
@@ -0,0 +1,24 @@
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+#include "render.h"
+#include <stdio.h>
+
+// TODO: hardcode the shaders
+static const char *vertexShader = "";
+static const char *fragmentShader = "";
+
+int compileShader() {
+
+}
+
+int createShaderProgram() {
+
+}
+
+int loadTexture() {
+
+}
+
+void renderQuad(int shaderProgram, int texture) {
+
+}
